fe45994226618f3ca1c42e0c0386347ba41ad394
random-accnt <jiri.capsky@seznam.cz>
2024-03-13 18:20:32 +0100

    added plugins for golang

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,4 +66,27 @@ return {
       { '<leader>sc', '<cmd>VenvSelectCached<cr>' },
     },
   },
+
+  -- [[ PROGRAMMING TOOLS ]]
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    'yanskun/gotests.nvim',
+    ft = 'go',
+    config = function()
+      require('gotests').setup()
+    end,
+  },
 }
